55adb777c8d4ef4f059481a469bc845a5898dcb2
samiu11ah <msamiullah295@gmail.com>
2022-11-26 13:24:07 +0500

    auth done

--- base/models.py
+++ base/models.py
@@ -1,13 +1,28 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 
 class Car(models.Model):
-    make = models.CharField(max_length=100)
-    model = models.CharField(max_length=100)
-    price = models.DecimalField(default=0.0, max_digits=10,decimal_places=2)
     image = models.ImageField(upload_to='cars/')
+    make = models.CharField(max_length=100) # tesla, toyota
+    model = models.CharField(max_length=100) # corola
+    # addtinal column
+    model_year = models.CharField(max_length=4, default='1960')
+
+    price = models.DecimalField(default=0.0, max_digits=10, decimal_places=2)
+
+    # additional columns
+    condition = models.CharField(max_length=100, choices=(('new', 'new'), ('old', 'old')))
+    condition_rating = models.CharField(max_length=100, blank=True, null=True, choices=(('10', '10/10'), ('9', '9/10'), ('8', '8/10'), ('7', '7/10'), ('6', '6/10'), ('5', '5/10'), ('4', '4/10'), ('3', '3/10'), ('2', '2/10'), ('1', '1/10')))
+    description = models.TextField(blank=True, null=True)
+    sold = models.BooleanField(default=False)
+    sold_to = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+
+
 
     def __str__(self):
-        return f'make: {self.make}, model: {self.model}'
+        return f'id: {self.id}, make: {self.make}, model: {self.model}, year: {self.model_year}'
--- car_dealership/settings.py
+++ car_dealership/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 import os
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -123,6 +124,18 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+LOGIN_REDIRECT_URL = 'index'
+
+from django.contrib.messages import constants as messages
+
+MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-info',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger',
+}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
