b6be01062fb5344a92c04ddca23e854cfd4d58a2
samiu11ah <msamiullah295@gmail.com>
2022-11-26 09:45:26 +0500

    basic Car model and some setup

--- base/models.py
+++ base/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Car(models.Model):
+    make = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+    price = models.DecimalField(default=0.0, max_digits=10,decimal_places=2)
+    image = models.ImageField(upload_to='cars/')
+
+    def __str__(self):
+        return f'make: {self.make}, model: {self.model}'
